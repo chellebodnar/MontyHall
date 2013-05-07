f4245a4cd9a3f797460f8291797006a246d85666
Michelle <chellebodnar@gmail.com>
2013-05-06 21:19:59 -0400

    Also does Monty Fall Simulation

--- hall.cpp
+++ hall.cpp
@@ -5,25 +5,62 @@ using namespace std;
 
 int main()
 {
-    cout << "Monty Hall Simulation" << endl << endl;
-    int rando = 0;
-    int car = 0;
-    int goat = 0;
-    int num = 10000;
-    for(int i = 0; i < num; i++)
+    while(true)
     {
-        rando = rand()%3 + 1;
-        if(rando == 1)
+        cout << "Monty Hall Variant Simulation" << endl << endl;
+        cout << "Select a type of simulation:" << endl;
+        cout << "h - Monty Hall Problem" << endl;
+        cout << "b - Banana Peel Variation" << endl;
+        //cout << "d - Door Preference Variation" << endl;
+        cout << "q - quit" << endl;
+
+        char type = 'h';
+        cin >> type;
+
+        if(type != 'h' && type != 'b' && type != 'q')
         {
-            goat++;
+            cout << "If you can't type, you're probably not ready for this simulation." << endl;
+            exit(1);
         }
-        else
+        if(type == 'q')
         {
-            car++;
+            cout << "Thanks for playing!" << endl;
+            break;
         }
+        int rando = 0;
+        int randHost = 0;
+        int car = 0;
+        int goat = 0;
+        int i = 0;
+        int num = 10000;
+
+        while(i < num)
+        {
+            rando = rand()%3 + 1;
+            randHost = rand()%2 + 2;
+            if(type == 'b')
+            {
+                if(rando == randHost)
+                {
+                    continue;
+                }
+            }
+
+            if(rando == 1)
+            {
+                goat++;
+            }
+            else
+            {
+                car++;
+            }
+            i++;
+        }
+
+        cout << "You have won " << car << " cars and " << goat << " goats." << endl;
+        cout << "Switching has been successful " << car*100/num  << "% of the time." << endl << endl << endl;
+
     }
-    cout << "You have won " << car << " cars and " << goat << " goats." << endl;
-    cout << "Switching has been successful " << car*100/num  << "% of the time." << endl;
 
     return 0;
 }
